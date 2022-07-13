cd06d21595d0fec94865b61f290fcec12d67850a
tynguyen199001 <tynguyen10001@gmail.com>
2022-07-13 22:21:30 +0700

    upload_frontend

--- antystoreshop/app/Http/Controllers/AdminController.php
+++ antystoreshop/app/Http/Controllers/AdminController.php
@@ -8,9 +8,5 @@ class AdminController extends Controller
 {
     public function index(){
         return view('admin.dashbroard');
-    }
-    public function index1(){
-        return view('admin.dashbroard');
-    }
-
+    }   
 }
